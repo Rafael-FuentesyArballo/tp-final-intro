d97214164c9f2e011a2c2d1c42c2b2fd3b122226
Rafael Fuentes y Arballo <rfuentes@fi.uba.ar>
2025-07-04 17:42:39 -0300

    Comienzo a definir endpoints en api

--- backend/src/api.js
+++ backend/src/api.js
@@ -7,3 +7,31 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server iniciado en puerto ${port}`)
 });
+
+
+//Endpoints usuarios//
+
+//get all usuarios
+app.get ('api/usuarios', (req,res) => {
+    res.json();
+});
+
+//get one usuario
+app.get ('api/usuarios/:id', (req,res) => {
+    res.json();
+});
+
+//insert usuario
+app.post('api/usuarios', (req,res) => {
+    res.json();
+});
+
+//delete usuario
+app.delete('api/usuarios/:id', (req,res) => {
+    res.json();
+});
+
+//delete usuario
+app.put('api/usuarios/', (req,res) => {
+    res.json();
+});
