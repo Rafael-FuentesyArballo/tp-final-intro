d5139948ae55e5db1100a97f3f9e5d3ca7335e57
Rafael Fuentes y Arballo <rfuentes@fi.uba.ar>
2025-07-04 02:03:26 -0300

    Creo rama para hacer la api

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+//const express = require('express')
+import express from "express";
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo')
-})
+const app = express();
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server iniciado en puerto ${port}`)
+});
