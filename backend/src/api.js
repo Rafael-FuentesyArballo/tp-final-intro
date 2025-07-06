9e1122a1a0f8ce1d3ae33e248aea816d4b4c4a6b
Rafael Fuentes y Arballo <rfuentes@fi.uba.ar>
2025-07-05 22:55:25 -0300

    Agrego endpoints y funciones para crear y borrar  usuarios

--- backend/src/api.js
+++ backend/src/api.js
@@ -10,9 +10,11 @@ app.listen(port, () => {
 
 // funciones de db.js
 const {
+    check_mail,
     get_all_usuarios,
-    get_one_usuario,
-    insert_usuario,
+    get_one_usuario_id,
+    get_one_usuario_nombre,
+    create_usuario,
     del_usuario,
     edit_usuario,
 } = require ('./scripts/db')
@@ -25,23 +27,67 @@ app.get ('api/usuarios', async (req,res) => {
     res.json(usuarios);
 });
 
-//get one usuario
+//get one usuario POR ID
 app.get ('api/usuarios/:id', async (req,res) => {
-    const usuario = await get_one_usuario(req.params.id);
-    if (!usuario){
+    const usuario = await get_one_usuario_id(req.params.id);
+    if ( usuario === undefined ){
         return res.status(404).json({Error: 'Usuario no encontrado'});
     }
       res.json(usuario);
 });
 
-//insert usuario
+//get one usuario POR NOMBRE
+app.get ('api/usuarios/:nombre_usuario', async (req,res) => {
+    const usuario = await get_one_usuario_nombre(req.params.nombre_usuario);
+    if ( usuario === undefined ){
+        return res.status(404).json({Error: 'Usuario no encontrado'});
+    }
+      res.json(usuario);
+});
+
+
+
+//crear usuario WIP, CREAR CATCHEO DE EXCEPCIONES
 app.post('api/usuarios', async (req,res) => {
-    res.json();
+    const nombre = req.body.nombre_usuario;
+    const contra = req.body.contraseña;
+    const mail = req.body.mail;
+    const fecha = req.body.fecha_creacion_usuario;
+    const rol = req.body.rol;
+    const karma = 0; //karma inicial 0
+    const articulos = 0;
+
+    if ( await get_one_usuario_nombre(nombre) !== undefined ){
+        res.json("Nombre en uso por otro usuario");
+        return res.sendStatus(400);
+    }
+    if (await check_mail(mail)){
+        res.json("Direccion de correo en uso por otro usuario");
+        return res.sendStatus(400);
+    }
+
+
+    const usuario = await create_usuario(nombre,contra,mail,fecha,rol,karma,articulos);
+
+    if (usuario === undefined ){
+        return res.sendStatus(500);
+    }else{
+        res.json(usuario);
+    }
 });
 
 //delete usuario
 app.delete('api/usuarios/:id', async (req,res) => {
-    res.json();
+    const usuario = await get_one_usuario_id(req.params.id);
+
+    if (usuario === undefined){
+        res.sendStatus(404).json("Usuario no encontrado");
+    }else{
+        resultado = await del_usuario(req.params.id);
+        if (resultado === undefined ){
+            res,sendStatus(500).json("Error al borrar el usuario");
+        }
+    }
 });
 
 //editar usuario
--- backend/src/scripts/db.js
+++ backend/src/scripts/db.js
@@ -11,12 +11,21 @@ const dbclient = new Pool({
 })
 await client.connect()
 
+async function check_mail(mail){
+    const result = await dbclient.query("SELECT mail FROM usuarios");
+    if (result.includes(mail)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 async function get_all_usuarios(){
     const response = await dbclient.query("SELECT * FROM usuarios");
     return response.rows;
 }
 
-async function get_one_usuario(id){
+async function get_one_usuario_id(id){
     const response = await dbclient.query("SELECT * FROM usuarios WHERE id = $1",[id]);
     if (response.rowcount === 0 ){
         return undefined;
@@ -26,12 +35,60 @@ async function get_one_usuario(id){
     }
 }
 
+async function get_one_usuario_nombre(nombre){
+    const response = await dbclient.query("SELECT * FROM usuarios WHERE  nombre_usuario = $1",[nombre]);
+    if (response.rowcount === 0 ){
+        return undefined;
+    }
+    else{
+        return response.rows[0];
+    }
+}
 
+async function create_usuario (
+    nombre_usuario,
+    contraseña,
+    mail,
+    fecha_creacion_usuario,
+    rol,
+    karma,
+    articulos_comprados){
+    try{
+        const response = await dbclient.query(
+            "INSERT INTO usuarios ( nombre_usuario,contraseña,mail,fecha_creacion_usuario,rol,karma,articulos_comprados) VALUES ($1,$2,$3,$4,$5,$6)",
+            [nombre_usuario,contraseña,mail,fecha_creacion_usuario,rol,karma,articulos_comprados]
+        );
+    } catch {
+        return undefined;
+    };
+
+    return{
+    nombre_usuario,
+    contraseña,
+    mail,
+    fecha_creacion_usuario,
+    rol,
+    karma,
+    articulos_comprados
+    };
+}
+
+async function del_usuario(id){
+    try{
+        await dbclient.query(
+            "DELETE FROM usuarios WHERE id = $1",[id]);
+    } catch {
+        return undefined;
+    };
+
+}
 
 module.exports = {
+    check_mail,
     get_all_usuarios,
-    get_one_usuario,
-    insert_usuario,
+    get_one_usuario_id,
+    get_one_usuario_nombre,
+    create_usuario,
     del_usuario,
     edit_usuario,
 };
