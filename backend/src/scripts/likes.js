7fb0f58e1ee71c05959faee44c6547acb453ab53
Thomas Acosta <thomasacosta61@gmail.com>
2025-07-11 19:01:20 -0300

    Agrego consulta de likes de un comentario

--- backend/src/scripts/likes.js
+++ backend/src/scripts/likes.js
@@ -12,3 +12,18 @@ const dbclient = new Pool({
 })
 await dbclient.connect()
 
+export async function get_all_likes(){
+    const response = await dbclient.query("SELECT * FROM likes");
+    return response.rows;
+}
+
+export async function get_likes_by_comment(id_comentario) {
+    const response = await dbclient.query(
+        `SELECT SUM(l.valor) AS total_likes
+         FROM likes l
+         INNER JOIN comentarios c ON l.id_comentario = c.id`,
+        [id_comentario]
+    );
+    return response.rows[0].total_likes || 0;
+}
+
