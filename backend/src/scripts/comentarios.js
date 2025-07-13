81803875c83f3bd32c23ad37d73577770b8c5651
Thomas Acosta <thomasacosta61@gmail.com>
2025-07-12 22:15:18 -0300

    Agrego consultas para crear comentarios hijos y padres en comentarios.js

--- backend/src/scripts/comentarios.js
+++ backend/src/scripts/comentarios.js
@@ -31,3 +31,29 @@ export async function get_all_articulos_id_usuario(id_usuario){
     const response = await dbclient.query("SELECT * FROM articulos WHERE id_usuario = $1",[id_usuario]);
     return response.rows;
 }
+
+export async function create_comentario_padre(id_autor, id_articulo, texto) {
+    try{
+        const response = await dbclient.query(
+            "INSERT INTO comentarios (id_autor, id_articulo, texto) VALUES ($1,$2,$3) returning *",
+            [id_autor, id_articulo, texto]
+        );
+        return response.rows[0];
+    } catch (err) {
+        console.error("Error en create_comentario_padre:", err.stack);
+    return undefined;
+    }
+}
+
+export async function create_comentario_hijo(id_autor, id_articulo, texto, id_comentario_padre) {
+    try{
+        const response = await dbclient.query(
+            "INSERT INTO comentarios (id_autor, id_articulo, texto, id_comentario_padre) VALUES ($1,$2,$3,$4) returning *",
+            [id_autor, id_articulo, texto, id_comentario_padre]
+        );
+        return response.rows[0];
+    } catch (err) {
+        console.error("Error en create_comentario_hijo:", err.stack);
+    return undefined;
+    }
+}
