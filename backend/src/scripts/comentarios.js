ab075d1a78c74208a2617558d08bc8e79c53a7cc
Thomas Acosta <thomasacosta61@gmail.com>
2025-07-12 22:08:04 -0300

    Agrego consulta para obtener un comentario en comentarios.js

--- backend/src/scripts/comentarios.js
+++ backend/src/scripts/comentarios.js
@@ -16,3 +16,13 @@ export async function get_all_comentarios(){
     const response = await dbclient.query("SELECT * FROM comentarios");
     return response.rows;
 }
+
+export async function get_one_comentario_id(id){
+    const response = await dbclient.query("SELECT * FROM comentarios WHERE id = $1",[id]);
+    if (response.rowCount === 0 ){
+        return undefined;
+    }
+    else{
+        return response.rows[0];
+    }
+}
