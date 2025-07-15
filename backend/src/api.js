f096dfe5b49671bff0c1da6099230fcb15f81744
lodiaz-555 <lodiaz@fi.uba.ar>
2025-07-14 22:02:27 -0300

    correccion en cors

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,10 +2,11 @@
 import express, { json } from "express";
 import cors from "cors";
 
-app.use(cors());
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 3000;
 
 app.listen(port, () => {
